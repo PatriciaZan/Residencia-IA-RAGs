fd3f59c6d61e0bee039ae427772985bc98a3a2e6
Patricia Zan <patricia.zan.oliveira@gmail.com>
2026-08-03 17:14:20 -0300

    Primeira aula - usando OpenIA token Key

---
+++ 1-ChamandoAPI/main.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+  api_key=os.getenv("API_KEY")
+)
+
+response = client.responses.create(
+  model="gpt-5.4-mini",
+  input="write a haiku about ai",
+  store=True,
+)
+
+print(response.output_text);
+
